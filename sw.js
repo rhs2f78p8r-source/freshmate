8d66c7e4b962a68aa6da2b5a9d770c0704dbfd54
rhs2f78p8r-source <rhs2f78p8r@privaterelay.appleid.com>
2026-05-22 09:42:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'freshmate-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에 있으면 반환, 없으면 네트워크 요청
+        return response || fetch(event.request);
+      })
+  );
+});
